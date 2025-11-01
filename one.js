a9113a22c45e0e37cc9b48262adac3c2f27eeb0b
kamransajjad004 <kamransajjadop@gmail.com>
2025-11-01 13:27:44 +0000

    practiced

---
+++ one.js
@@ -0,0 +1,74 @@
+// program to display the sum of natural numbers
+
+// let sum = 0;
+// const n = 100
+
+// // loop from i = 1 to i = n
+// // in each iteration, i is increased by 1
+// for (let i = 1; i <= n; i++) {
+//     sum += i;  // sum = sum + i
+// }
+
+// console.log(`sum: ${sum}`);
+
+// // Output: sum: 5050
+
+// const fruits = ["apple", "banana", "cherry"];
+
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+// }
+
+
+// let a = 10
+
+// while( a < 15){
+//     console.log("Hello");
+//     a++
+//     if(a == 13)
+//     break
+//     console.log("yo");
+// }
+
+// while( a < 16){
+//     console.log("Hello");
+//     a++
+//     if(a == 13)
+//     break
+//     console.log("yo");
+// }
+
+// let b = 5
+// console.time("Start")
+// while( b < 10){
+//     console.log("Hello");
+//     b++
+//     if(b == 9)
+//     break
+//     console.log("yo");
+// }
+// console.timeEnd("Start")
+
+// let q = 9
+// q = 100
+// console.log(q);
+
+// const w = 1
+// console.log(w);
+
+ let a=10
+ let b = 5
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a += b);
+// console.log(a == b);
+// console.log(a > b);
+// console.log(a -= b);
+// console.log(a % b);
+// console.log(a === "b");
+
+console.log(a + true);
+console.log(b + "");
+console.log(b + "F");
